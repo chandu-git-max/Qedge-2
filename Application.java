d0fdb9ac27b73620284f555a6bbfb7a608a165c7
chandu-git-max <bb4673249@gmail.com>
2024-08-26 21:20:52 +0530

    Feature202 changes by Ethan

--- Application.java
+++ Application.java
@@ -8,7 +8,32 @@ public class Addition
         Integer sum = Arrays.stream(args)
           .mapToInt(Integer::parseInt)
           .sum();
-        
+#Feature202 changes on reports module by Ethan in out2023
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        // Value of first is assigned to temporary
+        float temporary = first;
+
+        // Value of second is assigned to first
+        first = second;
+
+        // Value of temporary (which contains the initial value of first) is assigned to second
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
+#end Feature202 changes
         System.out.println(sum);
     }
 }
